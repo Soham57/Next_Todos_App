0a3b208b05a285077c8b05ba07305a1e0ec8eff3
soham57 <sohammalviya65@gmail.com>
2023-07-29 12:22:03 +0530

    getData function

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,3 +1,10 @@
+import db from "@/utils/db";
+
+const getData = async () => {
+  const todos = await db.todo.findMany({});
+  return todos;
+};
+
 const TodosPage = () => {
   return <div>Dashboard TodosPage </div>;
 };
