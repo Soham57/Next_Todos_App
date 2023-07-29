581b55e02687437708b5b957dbbd6ff3bd0d446a
soham57 <sohammalviya65@gmail.com>
2023-07-29 12:32:44 +0530

    TodoList component logic

---
+++ components/TodoList.tsx
@@ -0,0 +1,11 @@
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => {
+        <Todo todo={todo} key={todo.id} />;
+      })}
+    </div>
+  );
+};
