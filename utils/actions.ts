b2766428c56fc1ea36a11125e143bec918bb08ec
soham57 <sohammalviya65@gmail.com>
2023-07-30 00:36:59 +0530

    BUG fix for formData

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,7 @@ import db from "./db";
 export const newTodo = async (formData) => {
   const todo = await db.todo.create({
     data: {
-      content: formData.get(content),
+      content: formData.get("content"),
     },
   });
 };
