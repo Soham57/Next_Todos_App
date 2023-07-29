f62109b4511c218d0920e3f424a7cc210ac19ef6
soham57 <sohammalviya65@gmail.com>
2023-07-30 00:26:01 +0530

    Button for submission

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -5,6 +5,7 @@ export const NewTodoForm = () => {
     <div>
       <form action={newTodo}>
         <input name="content" type="text" className="border border-black/25" />
+        <button type="submit">new todo</button>
       </form>
     </div>
   );
