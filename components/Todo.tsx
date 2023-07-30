3e0f7894ce3ff0124d9692ce4737f12ecd2902cf
soham57 <sohammalviya65@gmail.com>
2023-07-30 09:45:06 +0530

    Todos.map bug fixed

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,4 +1,5 @@
 const Todo = ({ todo }) => {
+  console.log("in", todo.content);
   return <div>{todo.content}</div>;
 };
 export default Todo;
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,11 +1,12 @@
 import Todo from "./Todo";
 
 const TodoList = ({ todos }) => {
+  //console.log("tds:", todos);
   return (
     <div>
-      {todos.map((todo) => {
-        <Todo todo={todo} key={todo.id} />;
-      })}
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
