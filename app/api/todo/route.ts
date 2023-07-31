cf073401b95b971c9ded98f1cbee6c19f700fb17
soham57 <sohammalviya65@gmail.com>
2023-07-31 09:46:00 +0530

    Typo fix in GET

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-import db from '@/utils/db'
-export const GET = async (request:Request)=>{
-const todos= await debugger.todo.findMany({})
-return NextResponse.json({data:todos})
-} 
+import db from "@/utils/db";
+
+export const GET = async (request: Request) => {
+  const todos = await db.todo.findMany({});
+  return NextResponse.json({ data: todos });
+};
