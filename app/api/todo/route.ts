15a0ca44035d5e06cf2455b8f243e205a518fa64
soham57 <sohammalviya65@gmail.com>
2023-07-31 09:52:37 +0530

    Post verb for /todo api

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -5,3 +5,11 @@ export const GET = async (request: Request) => {
   const todos = await db.todo.findMany({});
   return NextResponse.json({ data: todos });
 };
+
+export const POST = async (request: Request) => {
+  const data = await request.json();
+  const todo = await db.todo.create({
+    data,
+  });
+  return NextResponse.json({ message: todo });
+};
